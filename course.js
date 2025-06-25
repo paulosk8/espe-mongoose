be33ad444cc45fc7d81ceb578db0fb7599b41993
Paulo Galarza <paulo-123@hotmail.es>
2025-06-25 09:19:07 -0500

    feat: Metodo create

--- course.js
+++ course.js
@@ -5,14 +5,34 @@ const courseSchema = new Schema({
   // _id: ObjectId: Identificador único de tu documento
   title: {
     type: String,
+    required: true, // Campo obligatorio
   },
-  description: String,
+  description: {
+    type: String,
+    minlength: [10, "No se cumple con la longitud mínima de 10 caracteres"], // Longitud mínima de 10 caracteres
+    maxlength: 300, // Longitud máxima de 500 caracteres
+  },
   numberOfTopics: {
     type: Number,
     default: 0, // Valor por defecto si no se proporciona
+    min: 0, // Valor mínimo permitido
+    max: 40, // Valor máximo permitido
   },
   publishedAt: Date,
 });
+
+courseSchema.virtual("info");
+
+/* 
+validate
+save
+remove
+updateOne
+DeleteOne
+init => async
+*/
+/* courseSchema.post('remove');
+courseSchema.pre('save'); */
 
 // 2. Crear el modelo
 const Course = mongoose.model("Course", courseSchema);
