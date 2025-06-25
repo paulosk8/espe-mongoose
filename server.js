f1a10000acc4f32d50480876086ba1c9b0c8a391
Paulo Galarza <paulo-123@hotmail.es>
2025-06-25 09:17:31 -0500

    fix: validation error de conexion

--- server.js
+++ server.js
@@ -3,19 +3,33 @@ const mongoose = require("mongoose");
 require("./course"); // Import the course model to ensure it's registered
 // Cadena de conexión con credenciales
 const connectionString =
-  "mongodb://admin:password123@localhost:27017/espe-mongoose";
-mongoose.connect(connectionString, () => {
-  try {
-    console.log("Conexión exitosa a MongoDB");
-  } catch (error) {
-    console.error("Error de conexión a MongoDB:", error);
-  }
-});
+  "mongodb://admin:password123@localhost:27017/espe-mongoose?authSource=admin";
+// Utilizando async/await para conectar a MongoDB
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Conexión exitosa a MongoDB"))
+  .catch((error) => console.error("Error de conexión a MongoDB:", error));
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.post("/course", async (req, res) => {
+  const Course = mongoose.model("Course");
+  Course.create({
+    title: "Curso de Mongoose",
+    description: "Curso de Mongoose para aprender a manejar bases de datos",
+    numberOfTopics: 10,
+  })
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((error) => {
+      console.log("Error al crear el curso:", error);
+      res.json(error);
+    });
 });
 
 app.listen(8080, () => console.log("Servidor iniciado"));
